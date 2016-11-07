d04a09939ae844356b9de11e39fe999802b9e675
Mark Banner <standard8@mozilla.com>
2016-11-07 15:16:03 +0000

    Use eslint config from example-webextension

--- scripts/addon-lint-consumer.js
+++ scripts/addon-lint-consumer.js
@@ -11,7 +11,6 @@
 
   license:  PUBLIC DOMAIN.
 
-
 //example .addonlinterrc
 ignorerules:
   LOW_LEVEL_MODULE: true
@@ -19,10 +18,10 @@ ignorerules:
 
 */
 
-var yamljs = require('yamljs');
+let yamljs = require("yamljs");
 
-function loadRules (fn) {
-  var ignored = {};
+function loadRules(fn) {
+  let ignored = {};
   try {
     ignored = (yamljs.load(fn)).ignorerules;
   } catch (err) {
@@ -32,42 +31,40 @@ function loadRules (fn) {
 }
 
 function filterLint(lint, ignored) {
-  ['errors', 'notices', 'warnings'].map(function (k) {
-    var filtered = lint[k].filter(function (seen) {
-      return !(seen.code in ignored);
-    });
+  ["errors", "notices", "warnings"].map(k => { // eslint-disable-line array-callback-return
+    let filtered = lint[k].filter(seen => !(seen.code in ignored));
     lint[k] = filtered;
   });
   return lint;
 }
 
 function output(filteredLint) {
-  var show = 0;
-  ['errors', 'notices', 'warnings'].map(function (k) {
+  let show = 0;
+  ["errors", "notices", "warnings"].map(k => { // eslint-disable-line array-callback-return
     if (filteredLint[k].length) {
       show = 1;
     }
   });
   if (show) {
-    console.error(filteredLint);
+    console.error(filteredLint); // eslint-disable-line no-console
   }
-  process.exit(show);
+  process.exit(show); // eslint-disable-line no-process-exit
 }
 
 function doTheWork(content) {
   // your code here
-  var ignored = loadRules('.addonlinterrc');
-  output(filterLint(JSON.parse(content),ignored));
+  let ignored = loadRules(".addonlinterrc");
+  output(filterLint(JSON.parse(content), ignored));
 }
 
 // read in all the stdin
-var content = '';
+let content = "";
 process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', function (buf) {
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", buf => {
   content += buf.toString();
 });
-process.stdin.on('end', function () {
+process.stdin.on("end", () => {
   // your code here
   doTheWork(content);
 });
