b07a5d5ad51c771036b2a76e28588590ad89f714
Mark Banner <standard8@mozilla.com>
2017-01-30 14:28:11 +0000

    Add enabling/disabling of telemetry.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/Standard8/search-reset-shield-study.svg?branch=master)](https://travis-ci.org/Standard8/search-reset-shield-study)
+
 Search Reset Shield Study
 
 An experimental add-on testing the search reset experience.
--- test/test-feature.js
+++ test/test-feature.js
@@ -1,5 +1,5 @@
 // var main = require("../");
-let {Cu} = require("chrome");
+let {Ci, Cu} = require("chrome");
 const {before, after} = require("sdk/test/utils");
 const {feature} = require("../lib/feature");
 
@@ -7,8 +7,10 @@ const {Services} = Cu.import("resource://gre/modules/Services.jsm", this);
 
 const searchBranch = "browser.search.";
 const searchResetPref = "reset.enabled";
+const searchTelemetryMirrorPref = "experiment.search-reset.telemetry.enabled.mirror";
+const telemetryEnabledPref = "toolkit.telemetry.enabled";
 
-let prefBranch = Services.prefs.getDefaultBranch(searchBranch);
+let prefBranch = Services.prefs.getBranch(searchBranch);
 
 exports["test installation - reset ui"] = function(assert) {
   feature.install("reset-ui");
@@ -22,20 +24,41 @@ exports["test installation - control"] = function(assert) {
   assert.ok(!prefBranch.getBoolPref(searchResetPref), "should not have set the preference to true");
 };
 
-exports["test cleanup"] = function(assert) {
+exports["test installation - enable telemetry"] = function(assert) {
+  feature.install("test");
+
+  assert.ok(Services.prefs.getBoolPref(telemetryEnabledPref), "should have enabled telemetry");
+  assert.ok(!prefBranch.getBoolPref(searchTelemetryMirrorPref), "should have saved the old telemtry pref");
+};
+
+exports["test uninstall"] = function(assert) {
+  Services.prefs.getDefaultBranch(searchBranch).setBoolPref(searchResetPref, false);
+
   feature.install("reset-ui");
 
-  feature.cleanup();
+  feature.uninstall();
 
   assert.ok(!prefBranch.getBoolPref(searchResetPref), "should have reset the preference");
 };
 
-exports["test eligible"] = function(assert) {
-  assert.ok(feature.isEligible(), "should be eligible by default");
+exports["test installation - reset telemetry"] = function(assert) {
+  feature.install("test");
 
+  feature.uninstall();
+
+  assert.ok(!Services.prefs.getBoolPref(telemetryEnabledPref), "should have reset telemetry");
+  assert.ok(prefBranch.getPrefType(searchTelemetryMirrorPref) === Ci.nsIPrefBranch.PREF_INVALID,
+    "should have cleared the old pref");
+};
+
+exports["test eligible"] = function(assert) {
   prefBranch.setBoolPref(searchResetPref, true);
 
   assert.ok(!feature.isEligible(), "should not be eligible if the pref is already true");
+
+  prefBranch.setBoolPref(searchResetPref, false);
+
+  assert.ok(feature.isEligible(), "should be eligible if the pref is false");
 };
 
 before(exports, () => {
